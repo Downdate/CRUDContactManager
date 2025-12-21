5b5598c7cf98ee7377c806aec128026f88745b24
Dan <Danielzareimain@gmail.com>
2025-12-21 16:13:53 +0330

    added DbContext and added SeedData Json

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Entities
@@ -9,19 +10,24 @@ namespace Entities
     /// </summary>
     public class Person
     {
+        [Key]
         public Guid ID { get; set; }
 
+        [StringLength(40)]
         public string? Name { get; set; }
 
+        [StringLength(40)]
         public string? EmailAddress { get; set; }
 
         public DateTime? DateOfBirth { get; set; }
 
+        [StringLength(10)]
         public String? Gender { get; set; }
 
         public Guid? CountryID { get; set; }
         public Country? Country { get; set; }
 
+        [StringLength(200)]
         public string? Address { get; set; }
 
         public bool ReceiveNewsLetters { get; set; }
